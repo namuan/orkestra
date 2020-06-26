49e56d3b9c60e5222e04e2b8c45a4d661d4ddd8b
namuan <575441+namuan@users.noreply.github.com>
2020-06-26 22:18:34 +0100

    https://github.com/namuan/orkestra/issues/2
    Update selected step in http step request controller

--- app/sections/httpstep/http_step_request_controller.py
+++ app/sections/httpstep/http_step_request_controller.py
@@ -5,7 +5,12 @@ class HttpStepRequestController:
 
         # domain events
         self.world.data.events.step_added.connect(self.on_step_added)
+        self.world.data.events.step_selection_changed.connect(self.on_step_selection_changed)
 
     def on_step_added(self, step_id):
         step_entity = self.world.step_store.get_step(step_id)
         self.parent.object_to_form(step_entity)
+
+    def on_step_selection_changed(self, step_id):
+        step_entity = self.world.step_store.get_step(step_id)
+        print("Currently Selected Id: {}".format(step_entity))
